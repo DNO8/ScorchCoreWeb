b5fe223b7f0719705673f0f2eb09e2154c6fdfa8
DANO <dcontrerasl@live.com>
2026-05-22 02:08:11 -0400

    fix: removiendo líneas en blanco innecesarias en layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,4 @@
 import type { Metadata } from "next";
-
-
 import { Cinzel_Decorative, Roboto_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
